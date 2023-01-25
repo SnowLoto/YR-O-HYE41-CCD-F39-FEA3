54db908c4d0805bf5a05348cde8ccd69c16c54b8
Liliya233 <abc572024594@gmail.com>
2023-01-25 19:49:02 +0800

    Token相关更新

--- launcher/fastbuilder/token.go
+++ launcher/fastbuilder/token.go
@@ -13,6 +13,14 @@ import (
 	"golang.org/x/term"
 )
 
+// 判断是否为token
+func isToken(token string) bool {
+	if strings.HasPrefix(token, "w9/BeLNV/") || strings.HasPrefix(token, "w9/abqFz/") {
+		return true
+	}
+	return false
+}
+
 // 加载现有的token
 func loadCurrentFBToken() string {
 	// 获取目录
@@ -36,8 +44,8 @@ func requestToken() string {
 	// 尝试加载现有的token
 	currentFbToken := loadCurrentFBToken()
 	// 读取成功, 提示是否使用
-	if currentFbToken != "" && strings.HasPrefix(currentFbToken, "w9/BeLNV/9") {
-		pterm.Info.Printf("要使用现有的 Fastbuilder 账户登录吗? 使用现有账户请输入 y, 使用新账户请输入 n: ")
+	if currentFbToken != "" && isToken(currentFbToken) {
+		pterm.Info.Printf("要使用当前设备储存的 Token 吗? 要请输入 y, 不要请输入 n: ")
 		if utils.GetInputYN() {
 			return currentFbToken
 		}
@@ -46,7 +54,8 @@ func requestToken() string {
 	pterm.Info.Printf("请输入 Fastbuilder 账号, 或者输入 Token: ")
 	Code := utils.GetValidInput()
 	// 输入token则直接返回
-	if strings.HasPrefix(Code, "w9/BeLNV/9") {
+	if isToken(Code) {
+		pterm.Success.Println("输入内容为 FBToken")
 		return Code
 	}
 	pterm.Info.Printf("请输入 Fastbuilder 密码 (不会回显): ")
