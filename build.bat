109ecb2f707f425a0af27e2dee9cf13b47b4efcf
Liliya233 <abc572024594@gmail.com>
2023-04-02 23:10:33 +0800

    目录调整

--- build.bat
+++ build.bat
@@ -2,28 +2,28 @@ SET CGO_ENABLED=0
     SET GOARCH=amd64
         :: Windows X86_64
         SET GOOS=windows
-        go build -ldflags="-w -s" -trimpath -o ../build/omega_launcher_windows_amd64.exe
+        go build -ldflags="-w -s" -trimpath -o ./build/omega_launcher_windows_amd64.exe
 
         :: Darwin X86_64
         SET GOOS=darwin
-        go build -ldflags="-w -s" -trimpath -o ../build/omega_launcher_darwin_amd64
+        go build -ldflags="-w -s" -trimpath -o ./build/omega_launcher_darwin_amd64
 
         :: Linux X86_64
         SET GOOS=linux
-        go build -ldflags="-w -s" -trimpath -o ../build/omega_launcher_linux_amd64
+        go build -ldflags="-w -s" -trimpath -o ./build/omega_launcher_linux_amd64
 
     SET GOARCH=arm64
         :: Windows arm64
         SET GOOS=windows
-        go build -ldflags="-w -s" -trimpath -o ../build/omega_launcher_windows_arm64.exe
+        go build -ldflags="-w -s" -trimpath -o ./build/omega_launcher_windows_arm64.exe
 
         :: Darwin arm64
         SET GOOS=darwin
-        go build -ldflags="-w -s" -trimpath -o ../build/omega_launcher_darwin_arm64
+        go build -ldflags="-w -s" -trimpath -o ./build/omega_launcher_darwin_arm64
 
         :: Linux arm64
         SET GOOS=linux
-        go build -ldflags="-w -s" -trimpath -o ../build/omega_launcher_linux_arm64
+        go build -ldflags="-w -s" -trimpath -o ./build/omega_launcher_linux_arm64
 
 :: 以下为安卓编译, 编译前请先指定ndk路径
 SET CGO_ENABLED=1
@@ -31,9 +31,9 @@ SET CGO_ENABLED=1
         :: Android arm64
         SET GOARCH=arm64
         SET CC=D:\android-ndk-r25c\toolchains\llvm\prebuilt\windows-x86_64\bin\aarch64-linux-android33-clang
-        go build -ldflags="-w -s" -trimpath -o ../build/omega_launcher_android_arm64
+        go build -ldflags="-w -s" -trimpath -o ./build/omega_launcher_android_arm64
 
         :: Android X86_64
         SET GOARCH=amd64
         SET CC=D:\android-ndk-r25c\toolchains\llvm\prebuilt\windows-x86_64\bin\x86_64-linux-android33-clang
-        go build -ldflags="-w -s" -trimpath -o ../build/omega_launcher_android_amd64
+        go build -ldflags="-w -s" -trimpath -o ./build/omega_launcher_android_amd64
