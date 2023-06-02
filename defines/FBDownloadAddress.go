6a2fb8c29838ae77e5c1d9a1247bc3ce8341060b
Liliya233 <79683806+Liliya233@users.noreply.github.com>
2023-06-02 18:05:31 +0800

    仓库列表更新

--- defines/FBDownloadAddress.go
+++ defines/FBDownloadAddress.go
@@ -8,13 +8,13 @@ var OFFICIAL_REPO = "https://github.com/LNSSPsd/PhoenixBuilder/releases/latest/d
 // 官方镜像仓库
 var OFFICIAL_REPO_MIRROR = "https://www.omega-download.top/https://github.com/LNSSPsd/PhoenixBuilder/releases/latest/download/"
 
-// 云裳公益镜像仓库
-var YSCLOUD_REPO = "https://data-1259150973.file.myqcloud.com/Mirror/FastBuilder/"
+// 海的官方镜像仓库
+var SEA_REPO = "https://likemc.xyz/build/"
 
-// rnhws-Team 仓库
+// rnhws-Team 预览版仓库
 var RNHWS_TEAM_REPO = "https://github.com/rnhws-Team/PhoenixBuilder/releases/latest/download/"
 
-// rnhws-Team 镜像仓库
+// rnhws-Team 预览版镜像仓库
 var RNHWS_TEAM_REPO_MIRROR = "https://www.omega-download.top/https://github.com/rnhws-Team/PhoenixBuilder/releases/latest/download/"
 
 // 本地 (自用)
--- fastbuilder/update.go
+++ fastbuilder/update.go
@@ -18,11 +18,11 @@ func selectRepo(cfg *defines.LauncherConfig, reselect bool) {
 		// 不再于列表提示自用仓库
 		utils.ConfPrinter.Println(
 			"当前可选择的仓库有：\n",
-			"1. 官方仓库\n",
+			"1. 官方仓库 (推荐)\n",
 			"2. 官方镜像仓库\n",
-			"3. 云裳公益镜像仓库\n",
-			"4. rnhws-Team 仓库\n",
-			"5. rnhws-Team 镜像仓库",
+			"3. 海的官方镜像仓库\n",
+			"4. rnhws-Team 预览版仓库\n",
+			"5. rnhws-Team 预览版镜像仓库",
 		)
 		cfg.Repo = utils.GetIntInputInScope("请输入序号来选择一个仓库", 1, 6)
 	}
@@ -34,13 +34,13 @@ func selectRepo(cfg *defines.LauncherConfig, reselect bool) {
 		pterm.Info.Printfln("将使用官方镜像仓库 (%s) 进行更新", defines.OFFICIAL_REPO_MIRROR)
 		STORAGE_REPO = defines.OFFICIAL_REPO_MIRROR
 	case 3:
-		pterm.Info.Printfln("将使用云裳公益镜像仓库 (%s) 进行更新", defines.YSCLOUD_REPO)
-		STORAGE_REPO = defines.YSCLOUD_REPO
+		pterm.Info.Printfln("将使用海的官方镜像仓库 (%s) 进行更新", defines.SEA_REPO)
+		STORAGE_REPO = defines.SEA_REPO
 	case 4:
-		pterm.Info.Printfln("将使用 rnhws-Team 仓库 (%s) 进行更新", defines.RNHWS_TEAM_REPO)
+		pterm.Info.Printfln("将使用 rnhws-Team 预览版仓库 (%s) 进行更新", defines.RNHWS_TEAM_REPO)
 		STORAGE_REPO = defines.RNHWS_TEAM_REPO
 	case 5:
-		pterm.Info.Printfln("将使用 rnhws-Team 镜像仓库 (%s) 进行更新", defines.RNHWS_TEAM_REPO_MIRROR)
+		pterm.Info.Printfln("将使用 rnhws-Team 预览版镜像仓库 (%s) 进行更新", defines.RNHWS_TEAM_REPO_MIRROR)
 		STORAGE_REPO = defines.RNHWS_TEAM_REPO_MIRROR
 	case 6:
 		pterm.Info.Printfln("将使用 本地仓库 (%s) 进行更新", defines.LOCAL_REPO)
