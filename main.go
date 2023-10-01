0ded86873ae485f025f2b57348df1cdd3f44ee35
SnowLotusLib <146601072+SnowLotusLib@users.noreply.github.com>
2023-10-01 12:13:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -50,9 +50,14 @@ func main() {
 	launcherConfig := &launcher.Config{}
 	utils.GetJsonData(filepath.Join(utils.GetCurrentDataDir(), "SnowConfig.json"), launcherConfig)
 	// 添加启动信息
-	pterm.DefaultBox.Println("https://Snow.fastbuilder.icu/SnowLotus/")
+	pterm.DefaultBox.Println("https://Snow.fastbuilder.icu/SnowLotus:11204")
 	pterm.Info.Println("Omega Launcher" + pterm.Yellow(" (Legacy Omega Only)") + pterm.Yellow(" (", string(version), ")"))
 	pterm.Info.Println("Author: CMA2401PT, Modified by SnowLotus")
+	_, err := net.Dial("tcp", "127.0.0.1:11024")
+	if err != nil {
+		pterm.Error.Println("未能连接到指定的验证服务器")
+		os.Exit(0)
+	}
 	// 询问是否使用上一次的配置
 	if fastbuilder.CheckExecFile() && launcherConfig.RentalCode != "" {
 		if result, _ := utils.GetInputYNInTime("是否使用上一次的登录配置？", 10); result {
